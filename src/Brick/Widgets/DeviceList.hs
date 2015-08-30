04a427e91d3f8c7f678d085debf1deaa5c1894b6
Hans-Peter Deifel <hpd@hpdeifel.de>
2015-08-31 00:48:49 +0200

    Begin port from vty-ui to brick
    Since the former is deprecated

---
+++ src/Brick/Widgets/DeviceList.hs
@@ -0,0 +1,96 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Brick.Widgets.DeviceList
+       ( newDeviceList
+       , renderDeviceList
+       , module Brick.Widgets.List
+       ) where
+
+import Brick
+import Brick.Widgets.List
+import DBus.UDisks2.Simple
+
+import qualified Data.Vector as V
+import Data.Vector (Vector)
+import qualified Data.Text as T
+import Data.Text (Text)
+import Data.Bool
+import Control.Lens
+
+newDeviceList :: Name -> [Device] -> List Device
+newDeviceList name devs = list name (V.fromList devs) itemHeight
+  where itemHeight = 1
+
+renderDeviceList :: List Device -> Widget
+renderDeviceList list = Widget Fixed Fixed $ do
+  let devs = listElements list
+
+  c <- getContext
+
+  let availableWidth = c ^. availWidthL
+
+      mountedWidth = minColumnWidth "Mounted" mountedColumn devs
+      notMountedWidth = positive $ availableWidth - mountedWidth
+
+      [nameWidth, fileWidth, mountPointWidth]
+        = expand notMountedWidth
+                 [ minColumnWidth "Name" nameColumn devs
+                 , minColumnWidth "Device" devFileColumn devs
+                 , minColumnWidth "Mount Point" mountPointColumn devs
+                 ]
+
+      renderRow selected dev = selAttr selected $
+                 hFix mountedWidth (txt $ mountedColumn dev)
+             <+> hFix nameWidth (txt $ nameColumn dev)
+             <+> hFix fileWidth (txt $ devFileColumn dev)
+             <+> hFix mountPointWidth (txt $ mountPointColumn dev)
+
+      selAttr True = withAttr listSelectedAttr
+      selAttr False = id
+
+      renderHeader =
+                 hFix mountedWidth (txt "Mounted")
+             <+> hFix nameWidth (txt "Name")
+             <+> hFix fileWidth (txt "Device")
+             <+> hFix mountPointWidth (txt "Mount Point")
+
+  render $ renderHeader <=> txt " " <=> renderList list renderRow
+
+hFix :: Int -> Widget -> Widget
+hFix width = hLimit width . padRight Max
+
+
+nameColumn :: Device -> Text
+nameColumn = devName
+
+devFileColumn :: Device -> Text
+devFileColumn = devFile
+
+mountPointColumn :: Device -> Text
+mountPointColumn dev
+  | V.null (devMountPoints dev) = " "
+  | otherwise = T.intercalate "," $ V.toList $ devMountPoints dev
+
+mountedColumn :: Device -> Text
+mountedColumn = bool " " "✔" . devMounted
+
+minColumnWidth :: Text -> (Device -> Text) -> (Vector Device) -> Int
+minColumnWidth header content devs = max maxWidth (T.length header) + 2
+  where maxWidth = V.maximum $ V.map (T.length . content) devs
+
+positive :: Int -> Int
+positive = max 0
+
+-- Distributes the free space over the individual elements
+expand :: Int -> [Int] -> [Int]
+expand total elems = elems'
+  where remaining = total - sum elems
+        num       = length elems
+        elems'    = zipWith (+)
+                      (toZero (remaining `rem` num))
+                      (map (+(remaining `div` num)) elems)
+
+
+toZero :: Int -> [Int]
+toZero 0 = repeat 0
+toZero n = n : toZero (n-1)
---
+++ src/MainBrick.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE LambdaCase, OverloadedStrings #-}
+module Main where
+
+import Brick
+import Brick.Widgets.DeviceList
+import Graphics.Vty hiding (Event)
+import qualified Graphics.Vty as Vty
+
+import DBus.UDisks2.Simple
+
+import qualified Data.Text.IO as T
+import System.Exit
+import System.IO
+import Control.Monad
+
+data AppState = AppState {
+  devList :: List Device
+}
+
+draw :: AppState -> [Widget]
+draw (AppState dl) = [w]
+  where w = renderDeviceList dl
+
+handler :: AppState -> Vty.Event -> (EventM (Next AppState))
+handler appState e = case e of
+  EvKey (KChar 'q') [] -> halt appState
+
+theme :: AttrMap
+theme = attrMap defAttr
+  [ (listSelectedAttr, defAttr `withBackColor` brightBlack) ]
+
+main :: IO ()
+main = do
+  (con,devs) <- connect >>= \case
+    Left err -> do
+      T.hPutStrLn stderr err
+      exitWith (ExitFailure 1)
+    Right x -> return x
+
+  let devList = listMoveTo 0 $ newDeviceList "devices" devs
+      app = App
+            { appDraw = draw
+            , appChooseCursor = neverShowCursor
+            , appHandleEvent = handler
+            , appStartEvent = return
+            , appAttrMap = const theme
+            , appLiftVtyEvent = id
+            }
+
+  void $ defaultMain app (AppState devList)

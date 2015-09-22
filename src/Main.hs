6827cc56c51aa2019daee44acd4b21c5a8e40796
Hans-Peter Deifel <hpd@hpdeifel.de>
2015-09-22 12:00:44 +0200

    Implement opening mount point in shell
    Bound to 's', this new features now opens a shell in the directory of
    the mount point of a device.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase, OverloadedStrings, RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
@@ -6,12 +7,13 @@ import Brick
 import Brick.Widgets.DeviceList
 import Brick.Widgets.Border
 import Brick.Widgets.HelpMessage
-import Graphics.Vty hiding (Event, nextEvent)
+import Graphics.Vty hiding (Event, nextEvent,(<|>))
 import qualified Graphics.Vty as Vty
 
 import DBus.UDisks2.Simple
 
 import qualified Data.Text.IO as T
+import qualified Data.Text as T
 import qualified Data.Vector as V
 import Data.Text (Text)
 import System.Exit
@@ -21,7 +23,14 @@ import Control.Concurrent
 import Data.Default
 import Data.Monoid
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Maybe
 import Data.Maybe
+import System.Environment
+import System.Posix.User
+import Control.Exception
+import System.IO.Error
+import Control.Applicative
+import System.Process
 
 data AppState = AppState {
   devList :: List Device,
@@ -46,7 +55,9 @@ helpMsg = KeyBindings
      , ("G, End", "Select last device")
      ])
   , ("Device operations",
-     [ ("RET", "Mount or unmount device")])
+     [ ("RET", "Mount or unmount device")
+     , ("s", "Open mount point in shell")
+     ])
   , ("Display settings",
      [ ("t", "Toggle display of system devices") ])
   ]
@@ -83,10 +94,13 @@ handler appState@AppState{..} e = case e of
           Just b  -> handleDialogKey b ev as
 
         handleListKey (EvKey KEnter []) as =
-          liftIO (mountUnmount as) >>= continue
+          liftIO (withSelected mountUnmount as) >>= continue
 
-        handleListKey (EvKey (KChar 't') []) as = do
+        handleListKey (EvKey (KChar 't') []) as =
           liftIO (toggleSystemDevices as) >>= continue
+
+        handleListKey (EvKey (KChar 's') []) as =
+          suspendAndResume (withSelected openShell as)
 
         handleListKey ev as = do
           lst' <- handleHJKLEvent ev devList
@@ -133,16 +147,19 @@ eventThread con chan = forever $ do
   ev <- nextEvent con
   writeChan chan (DBusEvent ev)
 
-mountUnmount :: AppState -> IO AppState
-mountUnmount as@AppState{..} = case listSelectedElement devList of
-  Nothing -> return $ showMessage as "No device selected"
-  Just (_, dev)
-    | devMounted dev  -> unmount connection dev >>= \case
-        Left err -> return $ showMessage as $ "error: " <> err
-        Right () -> return $ showMessage as "Device unmounted"
-    | otherwise       -> mount connection dev >>= \case
-        Left err -> return $ showMessage as $ "error: " <> err
-        Right mp -> return $ showMessage as $ "Device mounted at " <> mp
+----------------------------------------------------------------------
+-- Actions
+----------------------------------------------------------------------
+
+mountUnmount :: Device -> AppState -> IO AppState
+mountUnmount dev as
+  | devMounted dev = unmount (connection as) dev >>= \case
+      Left err -> return $ showMessage as $ "error: " <> err
+      Right () -> return $ showMessage as "Device unmounted"
+
+  | otherwise = mount (connection as) dev >>= \case
+      Left err -> return $ showMessage as $ "error: " <> err
+      Right mp -> return $ showMessage as $ "Device mounted at " <> mp
 
 
 toggleSystemDevices :: AppState -> IO AppState
@@ -151,6 +168,29 @@ toggleSystemDevices as = do
     con { configIncludeInternal = not $ configIncludeInternal con }
 
   return $ onList (listSimpleReplace $ V.fromList newDevList) as
+
+openShell :: Device -> AppState -> IO AppState
+openShell dev as = case devMountPoints dev V.!? 0 of
+  Nothing -> return $ showMessage as "Device not mounted. No mount point to open."
+  Just directory -> do
+    userShell <- getUserShell
+    res <- (callShell userShell >> return Nothing)
+      `catch` (\(e :: SomeException) -> return $ Just (show e))
+
+    return $ case res of
+      Nothing -> as
+      Just err -> showMessage as (T.pack err)
+
+      where callShell sh = do
+              (_,_,_,h) <- createProcess
+                           (proc sh []) { cwd = Just (T.unpack directory)
+                                        , delegate_ctlc = True
+                                        }
+              void $ waitForProcess h
+
+----------------------------------------------------------------------
+-- Helpers
+----------------------------------------------------------------------
 
 showMessage :: AppState -> Text -> AppState
 showMessage as msg = as { message = msg }
@@ -165,6 +205,38 @@ showHelp as = as { shownHelp = Just bindings }
 hideHelp :: AppState -> AppState
 hideHelp as = as { shownHelp = Nothing }
 
+-- | Returns the current shell of the user.
+--
+-- Considers this information in order:
+--   1. The SHELL environment variable
+--   2. The default shell of the user
+--   3. /bin/bash
+getUserShell :: IO FilePath
+getUserShell =
+  -- fromJust is OK here, because "/bin/bash" is always Just
+  fmap fromJust $
+    runMaybeT $ getEnvShell <|> getDefShell <|> return "/bin/bash"
+
+  where getEnvShell :: MaybeT IO FilePath
+        getEnvShell = ifExists $ getEnv "SHELL"
+
+        getDefShell :: MaybeT IO FilePath
+        getDefShell = do
+          uid <- liftIO getEffectiveUserID
+          user <- ifExists $ getUserEntryForID uid
+
+          return $ userShell user
+
+        ifExists :: IO a -> MaybeT IO a
+        ifExists action = MaybeT $ tryJust (guard . isDoesNotExistError) action >>= \case
+          Left _ -> return Nothing
+          Right res -> return (Just res)
+
 -- not onLisp!
 onList :: (List Device -> List Device) -> AppState -> AppState
 onList f appState = appState { devList = f (devList appState)}
+
+withSelected :: (Device -> AppState -> IO AppState) -> AppState -> IO AppState
+withSelected action as = case listSelectedElement (devList as) of
+  Nothing -> return $ showMessage as "No device selected"
+  Just (_, dev) -> action dev as
